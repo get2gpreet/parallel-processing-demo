0d6cbfd346ab693d9b82a8ae3b25705b36eb8fef
gurpreet <get2gpreet@gmail.com>
2022-10-16 01:08:09 +0530

    Adding test configuration

---
+++ src/test/java/DBUtilTest.java
@@ -0,0 +1,11 @@
+import org.example.model.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+public class DBUtilTest {
+
+
+}
